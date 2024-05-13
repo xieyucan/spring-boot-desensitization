76057183b63531bdd2fc453aeaed1d14ec56e44c
hui2.xie <hui2.xie@ly.com>
2024-05-13 14:04:30 +0800

    init

---
+++ src/main/java/com/xieahui/springboot/SpringBootDesensitizationApplication.java
@@ -0,0 +1,13 @@
+package com.xieahui.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDesensitizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDesensitizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xieahui/springboot/SpringBootDesensitizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xieahui.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDesensitizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
